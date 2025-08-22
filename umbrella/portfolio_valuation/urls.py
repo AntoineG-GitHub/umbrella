30f667be5907c11bde5da1bbec626348b524c07b
AntoineG-GitHub <antoinegeller@hotmail.be>
2025-08-22 16:49:47 +0200

    type endpoint

--- umbrella/portfolio_valuation/urls.py
+++ umbrella/portfolio_valuation/urls.py
@@ -4,6 +4,6 @@ from portfolio_valuation import views
 
 urlpatterns = [
     path("get_daily_portfolio_snapshot/", views.get_portfolio_valuations, name="get_daily_portfolio_snapshot"),
-    path("get_portfolio_stock/", views.get_portfolio_stock, name="get_portfolio_stock"),
+    path("get_portfolio_stocks/", views.get_portfolio_stock, name="get_portfolio_stocks"),
     path("user_snapshots/<int:user_id>/", UserShareSnapshotView.as_view(), name="user_snapshots")
 ]
