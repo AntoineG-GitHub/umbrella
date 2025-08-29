56bfe23f3c8f5526f8f815d8a703327aaac848ec
AntoineG-GitHub <antoinegeller@hotmail.be>
2025-08-29 16:45:02 +0200

    admin panel

--- umbrella/data_ingestion/admin.py
+++ umbrella/data_ingestion/admin.py
@@ -3,12 +3,14 @@ from .models import BaseHistoricalExchangeRate, HistoricalPrice, TickerInfo
 from django.contrib import admin
 from django.contrib import admin
 
+@admin.register(HistoricalPrice)
 class HistoricalPriceAdmin(admin.ModelAdmin):
     list_display = ('date', 'open', 'ticker')  # Add relevant fields here
     search_fields = ('date', 'ticker')  # Add searchable fields
     list_filter = ('ticker',)  # Add filters based on fields
     ordering = ('-date',)  # Order by date (most recent first)
 
+@admin.register(BaseHistoricalExchangeRate)
 class BaseHistoricalExchangeRateAdmin(admin.ModelAdmin):
     list_display = ('date', 'from_currency', 'to_currency', 'open')  
     search_fields = ('from_currency', 'to_currency') 
@@ -16,14 +18,11 @@ class BaseHistoricalExchangeRateAdmin(admin.ModelAdmin):
     ordering = ('-date',) 
     date_hierarchy = 'date'  
 
+@admin.register(TickerInfo)
 class TickerInfoAdmin(admin.ModelAdmin):
     list_display = ('ticker', 'name', 'sector', 'industry')
     search_fields = ('ticker', 'name', 'sector', 'industry')  
     list_filter = ('sector', 'industry')  
     ordering = ('ticker',) 
 
-
-admin.site.register(BaseHistoricalExchangeRate, BaseHistoricalExchangeRateAdmin)
-admin.site.register(HistoricalPrice, HistoricalPriceAdmin)
-admin.site.register(TickerInfo, TickerInfoAdmin)
 
--- umbrella/portfolio_valuation/admin.py
+++ umbrella/portfolio_valuation/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import DailyPortfolioSnapshot
+from portfolio_valuation.models import DailyPortfolioSnapshot, UserShareSnapshot
 
 @admin.register(DailyPortfolioSnapshot)
 class DailyPortfolioSnapshotAdmin(admin.ModelAdmin):
     list_display = ("date", "total_value", "total_units", "nav_per_unit")
+    ordering = ("-date",)
+    search_fields = ("date",)
+    list_filter = ("date",)
+
+@admin.register(UserShareSnapshot)
+class UserShareSnapshotAdmin(admin.ModelAdmin):
+    list_display = ("date", "user_id", "units_held", "value_held")
     ordering = ("-date",)
     search_fields = ("date",)
     list_filter = ("date",)
